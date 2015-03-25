2d6ba140fa7f9820c6250a32d33542094e4d755d
Nightindarkness <nightindarkness@gmail.com>
2015-03-25 23:41:20 +0100

    add words working

--- WCFServiceWebRole1/IService1.cs
+++ WCFServiceWebRole1/IService1.cs
@@ -14,34 +14,15 @@ namespace WCFServiceWebRole1
     {
 
         [OperationContract]
-        string GetData(int value);
+        string AddWord(string lang1, string lang2, string word1, string word2);
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        string ChangeWord(string word1, string word2);
 
-        // TODO: Add your service operations here
+        [OperationContract]
+        string DeleteWord(string word1, string word2);
     }
 
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+    
 }
--- WCFServiceWebRole1/Service1.svc.cs
+++ WCFServiceWebRole1/Service1.svc.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -12,22 +14,34 @@ namespace WCFServiceWebRole1
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+
+        private static SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=G:\Visual studio projekt\QUIZLANG\WCFServiceWebRole1\App_Data\lab7.mdf;Integrated Security=True");
+        private static SqlCommand com = new SqlCommand();
+        private SqlDataReader re;
+
+        
+
+        public string AddWord(string lang1, string lang2, string word1, string word2)
         {
-            return string.Format("You entered: {0}", value);
+            com.Connection = con;
+            con.Open();
+            com.CommandText = "insert into dictionary (lang1, lang2, word1, word2) values ('" + lang1 + "','" + lang2 + "','" + word1 + "','" + word2 + "')";
+            com.ExecuteNonQuery();
+            com.Clone();
+            con.Close();
+            //return string.Format("You entered: {0}", value);
+            return string.Format("You sucessfully your words to the database");
+
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public string ChangeWord(string word1, string word2)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            throw new NotImplementedException();
+        }
+
+        public string DeleteWord(string word1, string word2)
+        {
+            throw new NotImplementedException();
         }
     }
 }
